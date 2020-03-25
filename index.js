093d0fa31a1384a08e57b04399bc8483fac578b8
Sameer Bhat <sameerbhat76@gmail.com>
2020-03-25 23:58:08 +0530

    Changes

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ hive.reserve(function (err, connObj) {
         data.forEach(element => {
 
           var item = element ? element.replace(/[^\w\s]/gi, ' ') : null;
-          
+
           newdata.push(item);
         });
 
@@ -178,7 +178,7 @@ function insertItemsToDB(conn, tableName, data, callbackFunction, errorFunction)
       errorFunction(err);
     } else {
       // console.log("Executing query.");
-      statement.executeUpdate(`INSERT INTO table ${tableName} values ('${data[0]}','${data[1]}','${data[2]}','${data[3]}','${data[4]}','${data[5]}','${data[6]}','${data[7]}','${data[8]}','${data[9]}','${data[10]}','${data[11]}','${data[12]}','${data[13]}','${data[14]}','${data[15]}','${data[16]}','${data[17]}','${data[18]}','${data[19]}','${data[20]}','${data[21]}','${data[22]}','${data[23]}');`, function (
+      statement.executeUpdate(`INSERT INTO ${tableName} values ('${data[0]}','${data[1]}','${data[2]}','${data[3]}','${data[4]}','${data[5]}','${data[6]}','${data[7]}','${data[8]}','${data[9]}','${data[10]}','${data[11]}','${data[12]}','${data[13]}','${data[14]}','${data[15]}','${data[16]}','${data[17]}','${data[18]}','${data[19]}','${data[20]}','${data[21]}','${data[22]}','${data[23]}');`, function (
         err,
         resultset
       ) {
